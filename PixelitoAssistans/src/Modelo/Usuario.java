b14147065a41934e0afd8b0b2c8652ac77868a26
JeanJaramillo <81722915+JeanJaramillo@users.noreply.github.com>
2022-05-15 00:16:16 -0500

    Creación de la Usuario class

---
+++ PixelitoAssistans/src/Modelo/Usuario.java
@@ -0,0 +1,92 @@
+package Modelo;
+
+public class Usuario {
+    String nombre;
+    String apellidoPaterno;
+    int dni;
+    String correo;
+    String contraseña;
+    int idPeriodo;
+    int idCurso;
+    int idCarrera;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellidoPaterno, int dni, String correo, String contraseña, int idPeriodo, int idCurso, int idCarrera) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.dni = dni;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.idPeriodo = idPeriodo;
+        this.idCurso = idCurso;
+        this.idCarrera = idCarrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getIdPeriodo() {
+        return idPeriodo;
+    }
+
+    public void setIdPeriodo(int idPeriodo) {
+        this.idPeriodo = idPeriodo;
+    }
+
+    public int getIdCurso() {
+        return idCurso;
+    }
+
+    public void setIdCurso(int idCurso) {
+        this.idCurso = idCurso;
+    }
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+    
+    
+}
